1850585ef790e915ef2b7bf269b7ac84a052e311
alex-butcher <21243172+abutch3r@users.noreply.github.com>
2024-01-29 16:30:02 +0000

    Initial commit

--- src/main/java/io/openliberty/reactive/messaging/demo/app/ReactiveMessagingResource.java
+++ src/main/java/io/openliberty/reactive/messaging/demo/app/ReactiveMessagingResource.java
@@ -2,7 +2,6 @@ package io.openliberty.reactive.messaging.demo.app;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -12,6 +11,7 @@ import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import java.util.List;
+import java.util.concurrent.CompletionStage;
 
 @Path("/")
 @ApplicationScoped
@@ -27,10 +27,9 @@ public class ReactiveMessagingResource {
     @POST
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public String receiveMessage(String message){
+    public CompletionStage<Void> receiveMessage(String message){
         System.out.println("Received message "+ message);
-        emitter.send(message);
-        return "{\"message\":\"Received message "+ message +"\"}";
+        return emitter.send(message);
     }
 
     @GET
