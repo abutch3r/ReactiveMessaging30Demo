8fd1a95f6c0eabfb235cf35a76dc381a675d3aca
alex-butcher <21243172+abutch3r@users.noreply.github.com>
2024-01-30 15:08:17 +0000

    Add Initial Nack and Buffer endpoints
    Nack works if the message sent equals `alex`
    Buffersize doesn't seem to take effect at the moment, but only a small number of requests have been thrown at it.
    
    Add beta guard so that kafka read behavior is corrected. given acknowledgement strategies
    
    Updates to mp configuration to fix issues and other minor fixes

--- src/main/java/io/openliberty/reactive/messaging/demo/app/MessagingBean.java
+++ src/main/java/io/openliberty/reactive/messaging/demo/app/MessagingBean.java
@@ -3,7 +3,10 @@ package io.openliberty.reactive.messaging.demo.app;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class MessagingBean {
@@ -13,12 +16,34 @@ public class MessagingBean {
 
     @Incoming("kafka-message-in")
     @Outgoing("kafka-message-out")
-    public String processMessage(String message){
-        return "hello " + message;
+    public String processMessage(String name){
+        System.out.println("Saying hello to " + name);
+        return "hello " + name;
     }
 
     @Incoming("kafka-message-return")
     public void storeMessage(String message){
+        System.out.println("storing " + message);
         messageStore.storeMessage(message);
+    }
+
+    @Incoming("nack")
+    public CompletionStage<Void> nackMessage(Message message){
+        if (message.getPayload().equals("alex")){
+            return message.nack(new Exception("I don't like alex"));
+        } else {
+            return message.ack();
+        }
+    }
+
+    @Incoming("buffer")
+    public CompletionStage<Void> handleBufferedMessages(Message message){
+        System.out.println("Received buffered message " + message.getPayload());
+        try {
+            Thread.sleep(5000);
+            return message.ack();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/io/openliberty/reactive/messaging/demo/app/ReactiveMessagingResource.java
+++ src/main/java/io/openliberty/reactive/messaging/demo/app/ReactiveMessagingResource.java
@@ -2,6 +2,7 @@ package io.openliberty.reactive.messaging.demo.app;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -9,17 +10,28 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.OnOverflow;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 @Path("/")
-@ApplicationScoped
 public class ReactiveMessagingResource {
 
     @Inject
-    @Channel("message-in")
-    Emitter emitter;
+    @Channel("rest-message-in")
+    Emitter<String> emitter;
+
+    @Inject
+    @Channel("buffer")
+    @OnOverflow(value = OnOverflow.Strategy.BUFFER, bufferSize = 1)
+    Emitter<String> bufferedEmitter;
+
+    @Inject
+    @Channel("nack")
+    Emitter<String> nackEmitter;
 
     @Inject
     private MessageStore messageStore;
@@ -27,6 +39,7 @@ public class ReactiveMessagingResource {
     @POST
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_PLAIN)
     public CompletionStage<Void> receiveMessage(String message){
         System.out.println("Received message "+ message);
         return emitter.send(message);
@@ -37,6 +50,45 @@ public class ReactiveMessagingResource {
     @Path("/")
     public List<String> getMessages(){
         return messageStore.getMessages();
+    }
+
+    @POST
+    @Path("/nack")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public CompletionStage<Void>  receiveNackableMessage(String message){
+        CompletableFuture<Void> ackCf = new CompletableFuture<>();
+        nackEmitter.send(
+            Message.of(message,
+                () -> {
+                    ackCf.complete(null);
+                    return CompletableFuture.completedFuture(null);
+                }, t -> {
+                    ackCf.completeExceptionally(t);
+                    return CompletableFuture.completedFuture(null);
+                }
+            )
+        );
+        return ackCf;
+    }
+
+    @POST
+    @Path("/buffer")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public CompletionStage<Void>  receiveBufferedkableMessage(String message){
+        System.out.println("Processing buffered message " + message);
+        CompletableFuture<Void> ackCf = new CompletableFuture<>();
+        bufferedEmitter.send(
+                Message.of(message,
+                        () -> {
+                            ackCf.complete(null);
+                            return CompletableFuture.completedFuture(null);
+                        }, t -> {
+                            ackCf.completeExceptionally(t);
+                            return CompletableFuture.completedFuture(null);
+                        }
+                )
+        );
+        return ackCf;
     }
 
 }
